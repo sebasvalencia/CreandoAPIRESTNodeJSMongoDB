680ff567183307b601da658912720504981c4bb9
Sebastian Valencia <sebastian.valencian@gmail.com>
2017-09-09 17:37:00 -0500

    Instalacion de nodemon y body-parser

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
 
 //importamos express
 const express = require('express');
+const bodyParser = require('body-parser');
 
 //Crear el servidor
 const app = express();
+const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());//Respuesta en json
 
-app.listen(3000, () =>{
-  console.log('API REST corriendo en http:localhost:3000');
+app.listen(port, () =>{
+  console.log(`API REST corriendo en http:localhost:${port}`);
 })
