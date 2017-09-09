79f3e7b5b35670358aa8ee451363ecb3e4616078
Sebastian Valencia <sebastian.valencian@gmail.com>
2017-09-09 17:50:11 -0500

    Crear un enpoint con GET y parametros

--- index.js
+++ index.js
@@ -6,10 +6,26 @@ const bodyParser = require('body-parser');
 
 //Crear el servidor
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
+//Middleware
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());//Respuesta en json
+
+//Añadir las peticiones
+//get
+//app.get('/url_a_escuchar', devuelve callback)
+//en req esta la peticion y res la respuesta
+app.get('/hola/:name', (req, res) => {
+    res.send({mensaje: `Hola  ${req.params.name}`  });
+});
+
+
+
+
+
+
+
 
 app.listen(port, () =>{
   console.log(`API REST corriendo en http:localhost:${port}`);
