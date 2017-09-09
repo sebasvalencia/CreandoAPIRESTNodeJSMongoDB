7823a26431931473fcb2fd78a03b5215c28b7f3c
Sebastian Valencia <sebastian.valencian@gmail.com>
2017-09-09 18:31:14 -0500

    Cómo usar POSTMAN para testear tu API REST

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
 'use strict';
 
-//importamos express
 const express = require('express');
 const bodyParser = require('body-parser');
 
-//Crear el servidor
 const app = express();
 const port = process.env.PORT || 3001;
 
-//Middleware
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());//Respuesta en json
+app.use(bodyParser.json());
 
-//Añadir las peticiones
-//get
-//app.get('/url_a_escuchar', devuelve callback)
-//en req esta la peticion y res la respuesta
-app.get('/hola/:name', (req, res) => {
-    res.send({mensaje: `Hola  ${req.params.name}`  });
+//ecomerce tienda online
+//API REST sobre productos
+
+//GET todos los recursos
+app.get('/api/product',(req,res) => {
+  res.send(200, {products: []});
 });
 
+//GET un recurso especifico
+app.get('/api/product/:productId', (req, res) => {
+
+});
+
+//POST subir productos
+app.post('/api/product', (req,res) => {
+  console.log(req.body);//acceder al cuerpo del mensaje gracias a bodyParser
+  /*
+  { name: 'Mac',
+    prrice: '200',
+    photo: 'mack.png',
+    category: 'laptop' }
+  */
+  //res.send(200, {message: 'El producto se ha recibido'});
+  res.status(200).send({message: 'El producto se ha recibido'});
+});
+
+//PUT actualizar producto
+app.put('/api/product/:productId', (req, res) => {
+
+});
+
+//DELETE
+app.delete('/api/product/:productId',(req,res) =>{
+
+})
 
 
 
 
-
-
-
-app.listen(port, () =>{
+app.listen(port, () => {
   console.log(`API REST corriendo en http:localhost:${port}`);
 })
